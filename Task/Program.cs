e82f9993ff9129ef1411346b1b493adb27817c68
Olga Kosukhina <heeeelga@gmail.com>
2022-08-05 14:51:15 +0300

    Added solved task

---
+++ Task/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+string[] InitArray(int dimension)
+{
+    string[] array = new string[dimension];
+    Console.WriteLine("Введите строки:");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+
+    return array;
+}
+
+string[] GetNewArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[i] = array[i];
+        }
+    }
+
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != null)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+}
+
+Console.WriteLine("Введите размерность массива: ");
+int dimension = int.Parse(Console.ReadLine());
+string[] array = InitArray(dimension);
+string[] newArray = GetNewArray(array);
+PrintArray(newArray);
